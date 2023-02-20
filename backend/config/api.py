ebc1300193c59668b5629115575b968f65c5c909
Abdul-Basit-Ansari <abdulbasitansari288@gmail.com>
2023-02-20 12:33:56 +0500

    Edit API

--- backend/config/api.py
+++ backend/config/api.py
@@ -7,11 +7,11 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     # API server  config
     API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str = "Chatopea Platform"
+    PROJECT_NAME: str = "Authentication Platform"
     DESCRIPTION: str = '''
-    Chatopea Is Chating Platform 
+    Simple Authentication App
                     '''
-    SECRET_KEY: str = os.getenv('CHATOPEA_SECRET_KEY', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+    SECRET_KEY: str = os.getenv('APP_SECRET_KEY', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     ALGORITHM = "HS256"
     VERSION: str = "1.0.0"
     PASSWORD_LEN: int = 4
@@ -19,9 +19,9 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 1
     #init user
     INIT_USER = {
-        "username": os.getenv('CHATOPEA_INIT_USER_NAME', "admin"),
-        "fullname": os.getenv('CHATOPEA_INIT_USER_FULLNAME', "Super Administrator User"),
-        "email": os.getenv('CHATOPEA_INIT_USER_email', "admin@codebybasit.com")
+        "username": os.getenv('APP_INIT_USER_NAME', "admin"),
+        "fullname": os.getenv('APP_INIT_USER_FULLNAME', "Super Administrator User"),
+        "email": os.getenv('APP_INIT_USER_email', "admin@codebybasit.com")
     }
 
 settings = Settings()
