8fbd7980db89f093307f89c546ae80b4c5d84911
Abdul-Basit-Ansari <abdulbasitansari288@gmail.com>
2023-02-20 12:27:45 +0500

    Initial Commit

--- backend/config/database.py
+++ backend/config/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'mysql://root:Mysql$123@localhost/chatopea')
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'mysql://root:******@localhost/chatopea')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
